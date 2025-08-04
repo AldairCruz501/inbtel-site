f2cdf39de16f7ce8166dd47281dcb83e1bd1f19c
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2025-08-04 17:05:57 -0600

    Actualizacion del sitio web

--- src/data/planes.ts
+++ src/data/planes.ts
@@ -72,64 +72,64 @@ export const planesTriplePlay: PlanesData = {
     plansRes: [
         {
             numeroInscripcion: 1137231,
-            nombreTarifa: 'Paquete Residencial 80 Mbps simétrico + Plan TV Standard + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 80 Mbps simétrico + Plan TV Standard + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137234,
-            nombreTarifa: 'Paquete Residencial 150 Mbps simétrico + Plan TV Standard + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 150 Mbps simétrico + Plan TV Standard + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137244,
-            nombreTarifa: 'Paquete Residencial 200 Mbps simétrico + Plan TV Standard + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 200 Mbps simétrico + Plan TV Standard + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137249,
-            nombreTarifa: 'Paquete Residencial 300 Mbps simétrico + Plan TV Standard + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 300 Mbps simétrico + Plan TV Standard + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137254,
-            nombreTarifa: 'Paquete Residencial 600 Mbps simétrico + Plan TV Standard + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 600 Mbps simétrico + Plan TV Standard + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137258,
-            nombreTarifa: 'PPaquete Residencial 1000 Mbps simétrico + Plan TV Standard + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 1000 Mbps simétrico + Plan TV Standard + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },  
     ],
     plansGamer: [
         {
             numeroInscripcion: 1137155,
-            nombreTarifa: 'Paquete Residencial 80 Mbps + Plan TV Elite + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 80 Mbps + Plan TV Elite + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137124,
-            nombreTarifa: 'Paquete Residencial 150 Mbps + Plan TV Elite + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 150 Mbps + Plan TV Elite + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137172,
-            nombreTarifa: 'Paquete Residencial 200 Mbps + Plan TV Elite + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 200 Mbps + Plan TV Elite + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137177,
-            nombreTarifa: 'Paquete Residencial 300 Mbps + Plan TV Elite + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 300 Mbps + Plan TV Elite + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137176,
-            nombreTarifa: 'Paquete Residencial 600 Mbps + Plan TV Elite + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 600 Mbps + Plan TV Elite + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         },
         {
             numeroInscripcion: 1137216,
-            nombreTarifa: 'Paquete Residencial 1000 Mbps + Plan TV Elite + Llamadas ílimitadas a MX USA y CAN.',
+            nombreTarifa: 'Paquete Residencial 1000 Mbps + Plan TV Elite + Llamadas ilimitadas a MX USA y CAN.',
             fechaInscripcion: '15/03/2024',
         }, 
     ],
